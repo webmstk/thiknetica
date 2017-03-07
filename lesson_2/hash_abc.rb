b58400cd4404236fe8a53885538b2e519dcdefa4
trddddd <fcv3@ya.ru>
2017-03-07 16:11:16 +0000

    fix has_abc

--- lesson_2/hash_abc.rb
+++ lesson_2/hash_abc.rb
@@ -1,9 +1,7 @@
 # Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
-# просто говнокод( как научится писать красиво?потратил минут 50, по документации rubydoc, зато сам
 
-vowels = ('a'..'z').to_a
-number = (1..vowels.to_a.size).to_a
-
-vowels = vowels.zip(number).to_h
-puts vowels.select { |v| v =~ /[aeiouy]/ }
-
+alphabet = "a".."z"
+vowels = [ "a", "e", "i", "o", "u" ]
+hash_xyz = {}
+alphabet.each_with_index { |char, index| hash_xyz[index + 1] = char if vowels.include?(char) }
+puts hash_xyz
